3a8b011dff6c68a91808468cc262baf185158843
Mateo Rivera <mateo.rivera@udea.edu.co>
2023-06-03 01:39:11 -0500

    Feature: Testing create new academic subunit linked to an A.U.

---
+++ src/test/java/co/edu/udea/certification/calidad/runners/CreateNewAcademicSubunit.java
@@ -0,0 +1,14 @@
+package co.edu.udea.certification.calidad.runners;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/create_new_academic_unit.feature",
+        glue = "co.edu.udea.certification.calidad.stepdefinitions",
+        snippets = CucumberOptions.SnippetType.CAMELCASE
+)
+public class CreateNewAcademicUnit {
+}
---
+++ src/test/java/co/edu/udea/certification/calidad/stepdefinitions/CreateNewAcademicSubunitStepDefinitions.java
@@ -0,0 +1,50 @@
+package co.edu.udea.certification.calidad.stepdefinitions;
+
+import co.edu.udea.certification.questions.ISeeThreeOptionsForInvesting;
+import co.edu.udea.certification.questions.IsSuccessfulOperation;
+import co.edu.udea.certification.tasks.*;
+import co.edu.udea.certification.userinterfaces.UsuarioPage;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.WebDriver;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class CreateNewAcademicUnitStepDefinitions {
+    @Managed(driver = "chrome")
+    public WebDriver driver;
+    private Actor admin = Actor.named("LosSuperConocidos");
+
+    /*@Before
+    public void preStage(){
+        /*driver.manage().window().maximize();
+        investor.can(BrowseTheWeb.with(driver));
+    }*/
+
+    @Given("I am in the home page of PLA")
+    public void iAmInTheHomePage(){
+        // We maximize the window
+        driver.manage().window().maximize();
+
+        // The investor can surf through the browser
+        admin.can(BrowseTheWeb.with(driver));
+    }
+
+    @When("I go to academic unit form and fill it")
+    public void fillAcademicUnitForm(){
+        admin.attemptsTo(GoToAcademicUnitFormAndFill.solve(new UsuarioPage()));
+    }
+
+    @Then("I can save a new academic unit")
+    public void saveNewAcademicUnit(){
+        //admin.should(seeThat(ISeeThreeOptionsForInvesting.verify(),equalTo(true)));
+        admin.should(seeThat(IsSuccessfulOperation.verify(),equalTo(true)));
+    }
+}
